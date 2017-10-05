7afad0e739dda0462a299859f650981e0b7ed90a
Xiaojun Gao <gaoxiaojun@gmail.com>
2017-10-06 02:43:16 +0800

    ignore docxy gen files

---
+++ src/ux_amalg.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+ * Quantitative Kit Library                                                   *
+ *                                                                            *
+ * Copyright (C) 2017 Xiaojun Gao                                             *
+ *                                                                            *
+ * Distributed under the terms and conditions of the BSD 3-Clause License.    *
+ ******************************************************************************/
+
+#define UX_AMALG_C
+
+#include "ux_mem.h"
+#include "ux_datetime.h"
+#include "ux_spscq.h"
+#include "ux_mpscq.h"
+#include "ux_event.h"
+#include "ux_bus.h"
+#include "ux_dispatch.h"
+#include "ux_loop.h"
---
+++ test/unittest/test_loop.c
@@ -0,0 +1,17 @@
+#include "task.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "../src/ux_loop.h"
+#include <assert.h>
+
+
+TEST_IMPL(ux_loop_test)
+{
+    ux_loop_t loop;
+
+    ux_loop_init(&loop);
+
+    ux_loop_run(&loop, UX_RUN_NOWAIT);
+
+    return 0;
+}
