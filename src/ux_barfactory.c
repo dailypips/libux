264922c2e93b34a38b1f65ab7a75f5494bf7c3d8
Xiaojun Gao <gaoxiaojun@gmail.com>
2017-10-08 01:16:14 +0800

    part

--- src/ux_barfactory.c
+++ src/ux_barfactory.c
@@ -5,3 +5,25 @@
  *                                                                            *
  * Distributed under the terms and conditions of the BSD 3-Clause License.    *
  ******************************************************************************/
+
+#include "ux_internal.h"
+
+typedef struct ux_barfactory_s ux_barfactory_t;
+
+typedef struct ux_bar_item_s {
+    ux_barfactory_t* factory;
+    ux_instrument_t* instrument;
+    int provider_id;
+    ux_bar_type bar_type;
+    ux_bar_input bar_input;
+    long bar_size;
+    int session_enabled;
+    ux_timespan_t session1;
+    ux_timespan_t session2;
+} ux_bar_item_t;
+
+struct ux_barfactory_s {
+    ux_loop_t *framework;
+    void* item_by_instrument_id;
+    void* reminder_items;
+};
